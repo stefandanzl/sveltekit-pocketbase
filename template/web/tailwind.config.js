9dfbca22475513b8252873ad7ca3e458b6175815
Stefan Danzl <19376127+stefandanzl@users.noreply.github.com>
2024-03-25 00:52:42 +0100

    Update tailwind.config.js

--- template/web/tailwind.config.js
+++ template/web/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	theme: {},
-	plugins: [],
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/stwui/**/*.{svelte,js,ts,html}'
+	],
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('stwui/plugin')
+	]
 };
